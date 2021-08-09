281d5d6fe2164e1f3d6ffdfb5064c3468b92e8a2
Alias <75126400+codemeister12@users.noreply.github.com>
2021-08-09 10:25:29 -0400

    Update file_finder.sh

--- file_finder.sh
+++ file_finder.sh
@@ -1,1 +1,15 @@
+#! /bin/bash 
 
+echo "Enter the name of the first file :"
+read file_name
+
+echo "Enter the name of the second file :"
+read File_name
+
+
+if [[ -e $file_name && -e $File_name ]]
+then 
+ echo "$file_name and $File_name found"
+else
+ echo "files not found! "
+fi
